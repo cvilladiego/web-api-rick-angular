84de7504161ec360e37fa696f05ba6cecda7b3e7
Carlos Villadiego <carlovinar93@gmail.com>
2023-04-20 22:36:31 -0500

    se cargan las clases dependiendo el estado del personaje

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,9 +2,11 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
 import { CharactersComponent } from './componentes/characters/characters.component';
+import { EpisodesComponent } from './componentes/episodes/episodes.component';
 
 const routes: Routes = [
   {path:'characters', component:CharactersComponent, title:'Characters'},
+  {path:'episodes', component:EpisodesComponent, title:'Episodes'},
   {path:'**', component:CharactersComponent, title:'Characters'},
 
  
