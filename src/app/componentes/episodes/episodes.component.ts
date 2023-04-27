5a32ff8b5b30a0d8ce0732936b5a382b50ef2240
Carlos Villadiego <carlovinar93@gmail.com>
2023-04-26 21:33:12 -0500

    se listan los episodios

--- src/app/componentes/episodes/episodes.component.ts
+++ src/app/componentes/episodes/episodes.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { EpisodesService } from 'src/app/services/episodes.service';
 
 @Component({
   selector: 'app-episodes',
   templateUrl: './episodes.component.html',
   styleUrls: ['./episodes.component.scss']
 })
-export class EpisodesComponent {
+export class EpisodesComponent implements OnInit {
+  allEpisodes :any = [''];
+  constructor(private episodes: EpisodesService){ }
+
+
+  ngOnInit(): void {
+    this.episodes.getAllEpisodes()
+    .subscribe( episode => {
+      this.allEpisodes= episode;
+      console.log(this.allEpisodes); 
+    })
+    
+  }
+
+  
 
 }
